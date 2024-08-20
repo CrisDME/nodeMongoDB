080c07d7e5506c143ec4e3e8faf68d6c0590c2bc
CrisDME <cristiand_macias@soy.sena.edu.co>
2024-08-20 16:15:49 -0500

    actualizaciones

--- backend/models/usuarios.models.js
+++ backend/models/usuarios.models.js
@@ -12,10 +12,14 @@ const userSchema = new conexion.Schema({
         minLength: [5, 'La contraseña debe tener al menos 5 caracteres'],
         maxLength: [20, 'La contraseña debe tener como máximo 20 caracteres']
     },
+    fechaRegistro: {
+        type: Date,
+        default: Date.now,
+    },
     rol: {
         type: String,
-        required: true,
-        default: 'guest'
+        enum: ['Cliente', 'Administrador'],
+        default: 'Cliente',
     },
     cedula: {
         type: String,
